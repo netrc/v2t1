a148f26ddce43a07537103133be45c40f69a21b6
netrc <ric@netrc.com>
2020-03-26 14:13:38 -0400

    func w/ ident check

--- netlifyFuncs/hworld.js
+++ netlifyFuncs/hworld.js
@@ -1,7 +1,10 @@
 
 exports.handler = async (event, context) => {
+  const {identity, user} = context.clientContext;
+  const iStr = (identity) ? JSON.stringify(identity) : 'no i val'
+  const uStr = (user) ? JSON.stringify(user) : 'no u val'
   return {
     statusCode: 200,
-    body: "Hello, World - set via netlifyFuncs"
+    body: `Hello, World - set via netlifyFuncs...i:${iStr}   .....   u:${u}`
   };
 };
