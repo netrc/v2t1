56cb1e14e88ca923785c6f7b86847a39096fa2e7
netrc <ric@netrc.com>
2020-03-26 19:03:23 -0400

    corrected-env-var

--- netlifyFuncs/hworld.js
+++ netlifyFuncs/hworld.js
@@ -5,6 +5,6 @@ exports.handler = async (event, context) => {
   const uStr = (user) ? JSON.stringify(user) : 'no u val'
   return {
     statusCode: 200,
-    body: `Hello, World - VLCB_ENVA:${process.env.VLCB_ENVA} - set via netlifyFuncs...i:${iStr}   .....   u:${uStr}`
+    body: `Hello, World - VLCB_TABLE_NAME:${process.env.VLCB_TABLE_NAME} - set via netlifyFuncs...i:${iStr}   .....   u:${uStr}`
   };
 };
