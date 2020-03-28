b7061b456ff00b68ef03fce14485d0d2345a9b68
netrc <ric@netrc.com>
2020-03-28 13:52:59 -0400

    syntax typo in dtest

--- build.sh
+++ build.sh
@@ -6,18 +6,14 @@ mkdir -p ${D}
 cp index.html main.js ${D}
 
 cat > ${D}/env.js <<END_OF_HERE
-
-// this does not keep the strings secret
-// but it does keep them out of github
+// this does not keep the strings secret but it does keep them out of github
+// this file is only ready by index.html (and used in main.js)
 
 const VLCB = {
   AWS_ID: '$VLCB_AWS_ID',
   AWS_SECRET: '$VLCB_AWS_SECRET',
-  AWS_RW_ID: '$VLCB_RW_AWS_ID',
-  AWS_RW_SECRET: '$VLCB_RW_AWS_SECRET',
   TABLE_NAME: '$VLCB_TABLE_NAME'
 }
-
 END_OF_HERE
 
 echo doing funcs npm install
